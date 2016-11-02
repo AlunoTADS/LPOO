a8737d8f6424f01532797d7a287405abdb1ad641
LucasMarquesDePaula <marques.analista@gmail.com>
2016-11-02 17:44:18 -0200

    Implementação DaoI
    #choraBoy

--- SistemaAutomovel/src/br/ufpr/data/DaoI.java
+++ SistemaAutomovel/src/br/ufpr/data/DaoI.java
@@ -5,11 +5,22 @@
  */
 package br.ufpr.data;
 
+import java.util.List;
+
 /**
  *
  * @author Lucas
  * @param <T>
  */
 public interface DaoI<T> {
+     public void inserir(T t) throws Exception;
     
+    public void editar(T t) throws Exception;
+    
+    public void excluir(T t) throws Exception;
+    
+    public T buscar(T t) throws Exception;
+    
+    public List<T> listar(T t) throws Exception;
+
 }
