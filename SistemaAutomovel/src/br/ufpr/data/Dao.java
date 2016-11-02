5e97f9e56886432be3e5b39a1cb95975b977d9fb
LucasMarquesDePaula <marques.analista@gmail.com>
2016-11-02 17:26:32 -0200

    Adicionado tratamento de Exeção para Dao

--- SistemaAutomovel/src/br/ufpr/data/Dao.java
+++ SistemaAutomovel/src/br/ufpr/data/Dao.java
@@ -11,28 +11,34 @@ import java.sql.ResultSet;
  * @author Giovanni
  */
 public abstract class Dao {
-    	public static Connection con;
-	PreparedStatement stmt;
-	ResultSet rs;
-	CallableStatement call;
-	
-	private final String URL = "jdbc:sqlite:src/br/ufpr/data/db/database.db";
-	
-	public void open() throws Exception {
-            if(con == null || con.isClosed()) {
-		con = DriverManager.getConnection(URL);
-            }
-	}
-        
-	protected void close() throws Exception {
-            if(rs != null) {
-               rs.close(); 
-            }                
-            if(stmt != null) {
-                stmt.close();
-            }
-            if(con != null) {
-                con.close();
-            }
-	}
+
+    public static Connection con;
+    PreparedStatement stmt;
+    ResultSet rs;
+    CallableStatement call;
+
+    private final String URL = "jdbc:sqlite:src/br/ufpr/data/db/database.db";
+
+    public void open() throws Exception {
+        if (con == null || con.isClosed()) {
+            con = DriverManager.getConnection(URL);
+        }
+    }
+
+    protected void close() {
+        try {
+            rs.close();
+        } catch (Exception e) {
+        }
+
+        try {
+            stmt.close();
+        } catch (Exception e) {
+        }
+
+        try {
+            con.close();
+        } catch (Exception e) {
+        }
+    }
 }
