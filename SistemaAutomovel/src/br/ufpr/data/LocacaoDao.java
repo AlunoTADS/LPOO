3d7a16664a0614f0130e0be54cc05a4e3bb8d7a5
LucasMarquesDePaula <marques.analista@gmail.com>
2016-11-02 18:01:52 -0200

    Adicionado Schema

--- SistemaAutomovel/src/br/ufpr/data/LocacaoDao.java
+++ SistemaAutomovel/src/br/ufpr/data/LocacaoDao.java
@@ -1,9 +1,38 @@
 package br.ufpr.data;
 
+import br.ufpr.model.Locacao;
+import java.util.List;
+
 /**
  *
  * @author Lucas
  */
-public class LocacaoDao extends Dao implements DaoI<LocacaoDao> {
+public class LocacaoDao extends Dao implements DaoI<Locacao> {    
+
+    @Override
+    public void inserir(Locacao t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void editar(Locacao t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluir(Locacao t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Locacao buscar(Locacao t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Locacao> listar(Locacao t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
 }
