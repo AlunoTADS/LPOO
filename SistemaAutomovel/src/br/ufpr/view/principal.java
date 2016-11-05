8ad90980fc81db069715275b49e6bbd7f70adfbd
edenmoraski <edenmoraski@gmail.com>
2016-11-05 17:41:51 -0200

    Organizada Estrutura de Views

--- SistemaAutomovel/src/br/ufpr/view/principal.java
+++ SistemaAutomovel/src/br/ufpr/view/principal.java
@@ -1,5 +1,8 @@
 package br.ufpr.view;
 
+import br.ufpr.view.veiculo.jif_veiculo;
+import br.ufpr.view.cliente.jif_clientes;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
