960feb9bc57f2dbb9964debff6912f01f8448b36
LucasMarquesDePaula <marques.analista@gmail.com>
2016-11-05 16:26:43 -0200

    Adicionado SimpleReflectTable

--- SistemaAutomovel/src/br/ufpr/model/Cliente.java
+++ SistemaAutomovel/src/br/ufpr/model/Cliente.java
@@ -10,7 +10,7 @@ package br.ufpr.model;
  * @author Giovanni
  */
 public class Cliente {
-    
+
     private Integer idCliente;
     private String nome;
     private String sobrenome;
@@ -31,7 +31,17 @@ public class Cliente {
     public Cliente(Integer idCliente) {
         this.idCliente = idCliente;
     }
+
+    @Column(label = "Cód", position = 0)
+    public Integer getIdCliente() {
+        return this.idCliente;
+    }
     
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+    
+    @Column(label = "Nome", position = 1)
     public String getNome() {
         return nome;
     }
@@ -40,6 +50,7 @@ public class Cliente {
         this.nome = nome;
     }
 
+    @Column(label = "Sobrenome", position = 2)
     public String getSobrenome() {
         return sobrenome;
     }
@@ -48,6 +59,7 @@ public class Cliente {
         this.sobrenome = sobrenome;
     }
 
+    @Column(label = "CPF", position = 3)
     public String getCpf() {
         return cpf;
     }
@@ -56,6 +68,7 @@ public class Cliente {
         this.cpf = cpf;
     }
 
+    @Column(label = "RG", position = 4)
     public String getRg() {
         return rg;
     }
@@ -64,15 +77,21 @@ public class Cliente {
         this.rg = rg;
     }
 
+    @Column(label = "RG UF", position = 5)
+    public UnidadeFederativa getRgUF() {
+        return rgUF;
+    }
+
+    public void setRgUF(UnidadeFederativa rgUF) {
+        this.rgUF = rgUF;
+    }
+
+    @Column(label = "Endereço", position = 6)
     public String getEndereco() {
         return endereco;
     }
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
-    }    
-
-    public Integer getIdCliente() {
-        return this.idCliente;
     }
 }
--- SistemaAutomovel/src/br/ufpr/view/SimpleReflectTableModel.java
+++ SistemaAutomovel/src/br/ufpr/view/SimpleReflectTableModel.java
@@ -1,229 +1,79 @@
-/*
- * Created on 29/08/2007 00:30:07
- * 
- * History
- * =======
- *    Date    Description
- * ---------- ----------------------------------------------------------------------------
- *
- */
 package br.ufpr.view;
 
-//import com.elisha.commons.ui.table.t2.ReflectTableModelManager;
-
+import br.ufpr.model.Column;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.AbstractTableModel;
 
-//import java.lang.reflect.Method;
-//import java.util.List;
-//
-//import javax.swing.table.AbstractTableModel;
-//
-//import com.elisha.commons.util.StringUtil;
-//import com.elisha.commons.util.TypesUtil;
+public class SimpleReflectTableModel extends AbstractTableModel {
 
-/**
- * @author Douglas Granzotto
- * @version 1.0
- */
-public class ReflectTableModel extends AbstractTableModel {
+    private static final long serialVersionUID = 7367545292158107310L;
 
+    protected Map<Integer, Column> columns = new TreeMap<>();
+    protected Map<Integer, Method> methods = new TreeMap<>();
+    protected List dataList = new ArrayList();
+
+    public SimpleReflectTableModel(Class clazz) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Column.class)) {                
+                Column annotation = method.getAnnotation(Column.class);
+                columns.put(annotation.position(), annotation);
+                methods.put(annotation.position(), method);
+            }
+        }
+    }
+    
+    protected Column getColumn(int columnIndex) {
+        return columns.get(columnIndex);
+    } 
+
+    protected Method getMethod(int columnIndex) {
+        return methods.get(columnIndex);
+    }
+        
+    public void setDataList(List dataList) {
+        this.dataList = dataList;
+        fireTableDataChanged();
+    }
+
+    public List getDataList() {
+        return dataList;
+    }
+
+    
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return dataList.size();
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return columns.size();
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return String.format(getColumn(columnIndex).format(), getMethod(columnIndex).invoke(dataList.get(rowIndex)));
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(SimpleReflectTableModel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(SimpleReflectTableModel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(SimpleReflectTableModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
     }
-//
-//    private static final long serialVersionUID = 7367545292158107310L;
-//    
-//    protected Method[] getMethods = null;
-//    protected Method[] setMethods = null;
-//    protected String[] columnLabels = null;
-//    protected String[] columnNames = null;
-//    protected Class[] columnClasses = null;
-//    protected boolean[] editableColumns = null;
-//    protected Class clazz = null;
-//    protected List dataList = null;
-//    protected ReflectTableModelManager tableModelManager;
-//
-//    
-//    public ReflectTableModel(String columnNames[], Class clazz) {
-//        this(null, columnNames, null, null, clazz);
-//    }
-//
-//    public ReflectTableModel(String columnLabels[], String columnNames[], Class clazz) {
-//        this(columnLabels, columnNames, null, null, clazz);
-//    }
-//
-//    public ReflectTableModel(String columnLabels[], String columnNames[], Class columnClasses[], Class clazz) {
-//        this(columnLabels, columnNames, columnClasses, null, clazz);
-//    }
-//
-//    public ReflectTableModel(String[] columnLabels, String[] columnNames, Class[] columnClasses, boolean[] editableColumns, Class clazz) {
-//        try {
-//            this.columnLabels = columnLabels;
-//            this.columnNames = columnNames;
-//            this.columnClasses = columnClasses;
-//            boolean noColumnClasses = (columnClasses == null);
-//            if (noColumnClasses) {
-//                this.columnClasses = new Class[columnNames.length];
-//            }
-//            this.editableColumns = editableColumns;
-//            this.clazz = clazz;
-//            getMethods = new Method[columnNames.length];
-//            setMethods = new Method[columnNames.length];
-//            for (int i = 0; i < columnNames.length; i++) {
-//                String uField = StringUtil.toFirstLetterUpperCase(columnNames[i]);
-//                try {
-//                    getMethods[i] = clazz.getMethod("get" + uField, (Class[]) null);
-//                } catch (NoSuchMethodException e) {
-//                    getMethods[i] = clazz.getMethod("is" + uField, (Class[]) null);
-//                }
-//                Class rt = getMethods[i].getReturnType();
-//                setMethods[i] = clazz.getMethod("set" + uField, new Class[]{rt});
-//                if (noColumnClasses) {
-//                    if (rt.isPrimitive()) {
-//                        this.columnClasses[i] = TypesUtil.primitiveJavaClassToWrapperClass(rt);
-//                    } else {
-//                        this.columnClasses[i] = rt;
-//                    }
-//                }
-//            }
-//            this.tableModelManager = new ReflectTableModelManager();
-//            this.tableModelManager.setTableModel(this);
-//        } catch (SecurityException | NoSuchMethodException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    
-//    @Override
-//    public Class<?> getColumnClass(int columnIndex) {
-//        return columnClasses[columnIndex];
-//    }
-//
-//    @Override
-//    public String getColumnName(int columnIndex) {
-//        if (columnLabels != null) {
-//            return columnLabels[columnIndex];
-//        } else {
-//            return columnNames[columnIndex];
-//        }
-//    }
-//
-//    @Override
-//    public int getColumnCount() {
-//        return columnNames.length;
-//    }
-//
-//    @Override
-//    public int getRowCount() {
-//        return dataList.size();
-//    }
-//
-//    public Object getRow(int rowIndex) {
-//        return dataList.get(rowIndex);
-//    }
-//
-//    @Override
-//    public Object getValueAt(int rowIndex, int columnIndex) {
-//        Object value = null;
-//        try {
-//            Object object = dataList.get(rowIndex);
-//            value = getMethods[columnIndex].invoke(object, (Object[]) null);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return value;
-//    }
-//
-//    @Override
-//    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-//        try {
-//            Object object = dataList.get(rowIndex);
-//            setMethods[columnIndex].invoke(object, new Object[]{aValue});
-//            fireTableCellUpdated(rowIndex, columnIndex);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Override
-//    public boolean isCellEditable(int rowIndex, int columnIndex) {
-//        return tableModelManager.isCellEditable(rowIndex, columnIndex);
-//    }
-//
-//    public void addObject(Object object) {
-//        dataList.add(object);
-//        int row = dataList.size() - 1;
-//        fireTableRowsInserted(row, row);
-//    }
-//
-//    public void insertObjectAtRow(Object object, int rowIndex) {
-//        dataList.add(rowIndex, object);
-//        fireTableRowsInserted(rowIndex, rowIndex);
-//    }
-//    
-//    public void updateObjectAtRow(Object object, int rowIndex) {
-//        dataList.set(rowIndex, object);
-//        fireTableRowsUpdated(rowIndex, rowIndex);
-//    }
-//
-//    public void removeObjectAtRow(int rowIndex) {
-//        dataList.remove(rowIndex);
-//        fireTableRowsDeleted(rowIndex, rowIndex);
-//    }
-//
-//    public void refreshRow(int rowIndex) {
-//        fireTableRowsUpdated(rowIndex, rowIndex);
-//    }
-//
-//    public void refreshRows(int firstRowIndex, int lastRowIndex) {
-//        if (firstRowIndex < 0 || lastRowIndex < 0) {
-//            fireTableDataChanged();
-//        } else {
-//            fireTableRowsUpdated(firstRowIndex, lastRowIndex);
-//        }
-//    }
-//
-//    public void setDataList(List dataList) {
-//        this.dataList = dataList;
-//        fireTableDataChanged();
-//    }
-//
-//    public List getDataList() {
-//        return dataList;
-//    }
-//
-//    public void setEditableColumns(boolean[] editableColumns) {
-//        this.editableColumns = editableColumns;
-//    }
-//
-//    public void setColumnEditable(int columnIndex, boolean editable) {
-//        if (editableColumns != null && columnIndex > 0 && columnIndex < editableColumns.length) {
-//            editableColumns[columnIndex] = editable;
-//        }
-//    }
-//
-//    public void setTableModelManager(ReflectTableModelManager tableModelManager) {
-//        this.tableModelManager = tableModelManager;
-//        this.tableModelManager.setTableModel(this);
-//    }
-//    
-//    public int indexOf(Object object) {
-//        return dataList.indexOf(object);
-//    }
-//    
-//    public int lastIndexOf(Object object) {
-//        return dataList.lastIndexOf(object);
-//    }
-    
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return getColumn(columnIndex).label();
+    }
+
 }
