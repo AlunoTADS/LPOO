fdb2fe9c384f1629e036522f8652f9b89f826689
LucasMarquesDePaula <marques.analista@gmail.com>
2016-11-05 16:23:33 -0200

    Adicionado SimpleReflectTableModel

---
+++ SistemaAutomovel/src/br/ufpr/view/TestReflectTableModel.java
@@ -0,0 +1,62 @@
+package br.ufpr.view;
+
+import br.ufpr.model.Cliente;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Lucas
+ */
+public class TestReflectTableModel extends JPanel {
+
+    public TestReflectTableModel() {
+        super(new GridLayout(1, 0));
+        SimpleReflectTableModel simpleReflectTableModel = new SimpleReflectTableModel(Cliente.class);
+        JTable table = new JTable(simpleReflectTableModel);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+
+        simpleReflectTableModel.setDataList(Arrays.asList(new Cliente(WIDTH, TOOL_TIP_TEXT_KEY, TOOL_TIP_TEXT_KEY, TOOL_TIP_TEXT_KEY, TOOL_TIP_TEXT_KEY, TOOL_TIP_TEXT_KEY)));
+        
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        //Add the scroll pane to this panel.
+        add(scrollPane);
+    }
+
+    /**
+     * Create the GUI and show it. For thread safety, this method should be
+     * invoked from the event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("TableDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        TestReflectTableModel newContentPane = new TestReflectTableModel();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            createAndShowGUI();
+        });
+    }
+}
